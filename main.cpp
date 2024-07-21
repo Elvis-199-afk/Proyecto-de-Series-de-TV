eb93cb7c550e47666d447cd8c78736428e35782f
Elvis-199-afk <earocutipaq@unjbg.edu.pe>
2024-07-21 10:06:12 -0600

    Agregando la estructura del proyecto

---
+++ main.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+
+int main() {
+	int op;
+    do{
+        system("cls");
+        cout<<"=== GESTION DE SERIES DE TELEVISION ===\n\n";
+        cout<<" 1. Agregar serie\n";
+        cout<<" 2. Eliminar serie\n";
+        cout<<" 3. Mostrar lista general de series registradas\n";
+        cout<<" 4. Mostrar lista de series ordenadas por plataforma\n";
+        cout<<" 5. Salir del programa\n\n";
+        cout<<"== SELECCIONA UNA DE LAS OPCIONES ==\n";
+        cin>>op;
+        
+        switch (op){
+            case 1:
+                system("cls");
+                if(n==30){
+                    cout<<"Se ha llegado al limite de series\n";
+                }else{
+                    agregarSerie(series,n);
+                }
+                break;
+            case 2:
+                system("cls");
+                eliminarSerie(series,n);
+                cout<<"La lista despues de eliminar es: \n";
+                mostrarLista(series,n);
+                system("pause");
+                break;
+            case 3:
+                system("cls");
+                if(n==-1){
+                    cout<<"No hay series registradas...\n";    
+                }else{
+                    mostrarLista(series,n);
+                    cout<<endl;
+                }
+                system("pause");
+                break;
+            case 4:
+                system("cls");
+                if(n==-1){
+                    cout<<"No hay series registradas...\n";
+                }else{
+                    cout<<"El listado de series existentes, ordenado por plataforma es:\n\n";
+                    mostrarListaPorPlataforma(series,n);
+                }
+                system("pause");
+                break;
+            case 5:
+                cout<<"\nFIN . . .\n";
+                break;
+            default :
+                cout<<"\n\nINGRESE UNA OPCION VALIDA\n";
+                system("pause");
+                break;
+        }
+    } while(op != 5);
+
+	return 0;
+}
