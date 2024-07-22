594530d841764e108ac7fabb489b41af9c52ba72
Elvis-199-afk <earocutipaq@unjbg.edu.pe>
2024-07-22 10:04:02 +0700

    Agregando series ya establecidas

--- main.cpp
+++ main.cpp
@@ -2,9 +2,18 @@
 #include"SerieTV.h"
 using namespace std;
 int main() {
-	SerieTV series[30];
+	SerieTV series[30]={
+        {"Stranger Things", "Ciencia Ficcion", 2016, 4, "Netflix"},
+        {"The Mandalorian", "Aventura", 2019, 2, "Disney+"},
+        {"The Boys", "Accion", 2019, 3, "Amazon Prime"},
+        {"Breaking Bad", "Drama", 2008, 5, "Netflix"},
+        {"The Crown", "Historia", 2016, 4, "Netflix"},
+        {"The Marvelous Mrs. Maisel", "Comedia", 2017, 4, "Amazon Prime"},
+        {"WandaVision", "Superheroes", 2021, 1, "Disney+"},
+        {"The Expanse", "Ciencia Ficcion", 2015, 6, "Amazon Prime"}
+	};
     int op;
-    int n=-1;
+    int n=7;
     do{
         system("cls");
         cout<<"=== GESTION DE SERIES DE TELEVISION ===\n\n";
