4575ed7bdb7c51ec50af2ff9419333be5e8bdfc4
Franklin Anahua <franklinanahua5@gmail.com>
2024-07-21 21:39:31 -0500

    agregando eliminarSerie.cpp para eliminar una serie en especifico

---
+++ eliminarSerie.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<iomanip>
+#include<string.h>
+#include"SerieTV.h"
+using namespace std;
+void eliminarSerie(SerieTV *series, int &n){
+    int indice;
+    cout << "Lista de series:\n";
+    for(int i=0;i<=n;i++){
+        cout<<i+1<<". "<<series[i].titulo<<endl;
+    }
+    cout<<"Ingrese el numero de la posicion de la serie que desea eliminar: ";
+    cin>>indice;
+    for(int i=indice-1;i<n;i++){
+        series[i]=series[i+1];
+    }
+    n--;
+}
